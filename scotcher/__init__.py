cd0fbf9661fbbf50e32a9aea66e3dc678de4d620
ToniCipriani <tonicipriani@gmail.com>
2019-04-14 22:19:41 -0400

    fixed user session pre-request, added index register

--- scotcher/__init__.py
+++ scotcher/__init__.py
@@ -27,7 +27,11 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from. import auth
+    from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import listing
+    app.register_blueprint(listing.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
--- scotcher/auth.py
+++ scotcher/auth.py
@@ -32,15 +32,15 @@ def register():
             user_id = db_cur.fetchone()
             if user_id is not None:
                 error = 'User {} is already registered'.format(username)
-
         if error is None:
             db_cur.execute(
                 'INSERT INTO tb_user (username, password) VALUES (%s, %s)',
                 (username, generate_password_hash(password))
                 )
             db_conn.commit()
+            db_cur.close()
             return redirect(url_for('auth.login'))
-
+        db_cur.close()
         flash(error)
 
     return render_template('auth/register.html')
@@ -58,6 +58,7 @@ def login():
             'SELECT username,password FROM tb_user WHERE username = %s', (username,)
         )
         user = db_cur.fetchone()
+        db_cur.close()
 
         if user is None:
             error = 'Incorrect username.'
@@ -72,6 +73,22 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    """Add user to session"""
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db_conn = conn_sql()
+        db_cur = db_conn.cursor()
+        db_cur.execute(
+            'SELECT * FROM tb_user WHERE username = %s', (user_id,)
+        )
+        g.user = db_cur.fetchone()
+        db_cur.close()
 
 @bp.route('/logout')
 def logout():
